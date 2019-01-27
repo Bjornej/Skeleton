f442868c4d85a11e34e11bbf61b71ada796bc0f6
Paolo Nicodemo <paolonicodemo@gmail.com>
2019-01-27 21:27:43 +0100

    Aggiunta gestione pubblicazione messaggi

--- Source/Framework/CommandHandler.cs
+++ Source/Framework/CommandHandler.cs
@@ -6,6 +6,16 @@ namespace Framework
 {
     public abstract class CommandHandler<T> : ICommandHandler<T> where T : ICommand
     {
+        /// <summary>
+        ///     Contesto di esecuzzione corrente se eseguito dal bus
+        /// </summary>
+        protected NServiceBus.IMessageHandlerContext Context { get; set; }
+
+        /// <summary>
+        ///     Istanza dell' endpoint 
+        /// </summary>
+        public NServiceBus.IEndpointInstance EndpointInstance { get; set; }
+
         public abstract Task Apply(T command);
 
         public async Task<ExecutionResult> Execute(T command)
@@ -28,7 +38,43 @@ namespace Framework
 
         public Task Handle(T message, NServiceBus.IMessageHandlerContext context)
         {
+            Context = context;
             return Execute(message);
+        }
+
+
+        protected Task PublishAsync(IEvent evt)
+        {
+            if (Context != null)
+            {
+                return Context.Publish(evt, new NServiceBus.PublishOptions()
+                {
+
+                });
+            }
+            else
+            {
+                return EndpointInstance.Publish(evt, new NServiceBus.PublishOptions()
+                {
+                });
+            }
+        }
+
+        protected Task SendAsync(ICommand command)
+        {
+            if (Context != null)
+            {
+                return Context.Send(command, new NServiceBus.SendOptions()
+                {
+                    
+                });
+            }
+            else
+            {
+                return EndpointInstance.Send(command, new NServiceBus.SendOptions()
+                {
+                });
+            }
         }
     }
 }
